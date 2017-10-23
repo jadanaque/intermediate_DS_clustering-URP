18cda11fe8337c1869863dd9a181cca27ad17f27
jadanaque <javieradanaque@gmail.com>
2017-10-23 08:46:09 -0500

    Practicing pam and kmeans methods for clustering

--- personal_clustering_practice.R
+++ personal_clustering_practice.R
@@ -53,3 +53,49 @@ a4 <- aggregate(carsb[, -c(1, 2)], list(groups_4), median)
 data.frame(Cluster = a4$Group.1, Size = as.vector(table(groups_4)), a4[, -1])
 
 # Partitioning around medoids ----
+library(cluster)
+cars_pam <- pam(dist(cars_use), 3)  # cars_dist <- dist(cars_use)
+
+names(cars_pam)
+table(groups_3, cars_pam$clustering)
+
+# Which one is differing with our hclust model?
+carsb$Car[groups_3 != cars_pam$clustering]
+
+# Which cars are representative (typical) of their clusters? Medoids
+carsb$Car[cars_pam$id.med]
+carsb[cars_pam$id.med, ]
+
+# Silhouette plot
+plot(cars_pam)  # for me, best model: pam with 3 clusters
+
+# Now, one more pam cluster analysis using 4 clusters
+cars_pam4 <- pam(dist(cars_use), 4)
+plot(cars_pam4)
+
+# To plot the silhouette for a different cluster analysis, use the silhouette function
+plot(silhouette(cutree(cars_hclust, 4), dist(cars_use)))
+
+
+# Checking the optimal number of clusters
+wss <- (nrow(cars_use)-1)*sum(apply(cars_use,2,var))  # Total_SS
+
+medias1 <- apply(cars_use, 2, mean)
+sum(apply(cars_use, 1, function(obs){sum((obs - medias1)^2)}))  # Should be the same: Total_SS
+
+# For each number of clusters, compute kmeans
+for (i in 2:15) wss[i] <- kmeans(cars_use, centers=i)$tot.withinss  # all the sum can be replaced by 'kmeans(.)$tot.withinss
+plot(1:15, wss, type="b",
+     xlab="Nummero de Clusters",
+     ylab="Suma de cuadrados within", col = 4)
+
+# 3 appears to be an optimal number of clusters
+
+# Let's try a kmeans cluster analysis using 3 clusters:
+cars_kmeans <- kmeans(cars_use, centers = 3, iter.max=20)
+plot(silhouette(cars_kmeans$cluster, dist(cars_use)))
+
+table(cars_kmeans$cluster)
+
+table(groups_3, cars_kmeans$cluster)
+table(cars_pam$clustering, cars_kmeans$cluster)
